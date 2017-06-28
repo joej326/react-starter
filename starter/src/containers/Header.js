102815fd786ed4fce2e7c8b47fb583a617fd80bb
Joseph Joyce <joej326@gmail.com>
2017-06-27 18:21:23 -0600

    initialize

---
+++ starter/src/containers/Header.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+// import { connect } from 'react-redux';
+
+
+
+
+export default class Header extends Component{
+  constructor(props){
+    super(props);
+  }
+
+  render(){
+    return (
+      <div>
+        i am the header
+      </div>
+    )
+  }
+
+}
---
+++ starter/src/index.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import { BrowserRouter, Route } from 'react-router-dom';
+import ReduxPromise from 'redux-promise';
+
+import reducers from './reducers';
+import App from './App';
+
+const createStoreWithMiddleware = applyMiddleware(ReduxPromise)(createStore);
+
+
+
+ReactDOM.render(
+  <Provider store={createStoreWithMiddleware(reducers)}>
+    <BrowserRouter>
+      <div>
+        <App />
+      </div>
+    </BrowserRouter>
+  </Provider>
+  , document.getElementById('root'));
